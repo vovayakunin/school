bb56dc5ffc48590a3ac25f2ee4ede9899453726d
Vladimir <vovaykunin@gmail.com>
2023-08-21 10:31:30 +0300

    Дз3.5

---
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -0,0 +1,68 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.AvatarRepository;
+import ru.hogwarts.school.repository.StudentRepository;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class AvatarService {
+    private final AvatarRepository avatarRepository;
+    private final StudentRepository studentRepository;
+    @Value("${path.to.avatars.folder}")
+    private Path pathToAvatars;
+
+
+    public AvatarService(AvatarRepository avatarRepository, StudentRepository studentRepository) {
+        this.avatarRepository = avatarRepository;
+        this.studentRepository = studentRepository;
+    }
+
+    public Avatar getById(Long id) {
+        return avatarRepository.findById(id).orElseThrow();
+    }
+
+    public Long save(Long studentId, MultipartFile multipartFile) throws IOException {
+        String absolutePath = saveToDisk(studentId, multipartFile);
+        Avatar avatar = saveToDb(studentId, multipartFile, absolutePath);
+
+        return avatar.getId();
+
+    }
+
+    private String saveToDisk(Long studentId, MultipartFile multipartFile) throws IOException {
+        Files.createDirectories(pathToAvatars);
+        String originalFilename = multipartFile.getOriginalFilename();
+        int dotIndex = originalFilename.lastIndexOf(".");
+        String extension = originalFilename.substring(dotIndex);
+        String fileName = studentId + extension;
+        String absolutePath = pathToAvatars.toAbsolutePath() + "/" + fileName;
+        FileOutputStream fos = new FileOutputStream(absolutePath);
+        multipartFile.getInputStream().transferTo(fos);
+        fos.close();
+        return absolutePath;
+    }
+
+    private Avatar saveToDb(Long studentId, MultipartFile multipartFile, String absolutePath) throws IOException {
+        Student studentReference = studentRepository.getReferenceById(studentId);
+        Avatar avatar = avatarRepository.findByStudent(studentReference)
+                .orElse(new Avatar());
+
+        avatar.setStudent(studentRepository.getReferenceById(studentId));
+        avatar.setFilePath(absolutePath);
+        avatar.setMediaType(multipartFile.getContentType());
+        avatar.setFileSize(multipartFile.getSize());
+        avatar.setData(multipartFile.getBytes());
+        avatarRepository.save(avatar);
+
+        return avatar;
+    }
+}
